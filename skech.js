1208c87f2c247afab90adf1883ebe5ce565c45e0
Aakash Kavediya <kavediyaaakash@gmail.com>
2021-05-21 18:01:12 +0530

    let's see

--- skech.js
+++ skech.js
@@ -1,31 +1,68 @@
-var player, target, obs1, obs2,obs3,obs4,obs5,obs6,obs7,obs8, edge,worm,score,wormgroup;
+var player, target, obs1, obs2,obs3,obs4,obs5,obs6,obs7,obs8, edge,worm,score,wormgroup, background,wormImage,swampImage,playerImage,backImage;
+
+function preload() {
+
+  wormImage = loadImage("image/m1.png");
+  swampImage = loadImage("image/m2.png");
+  playerImage = loadImage("image/m3.png");
+  backImage = loadImage("image/pngegg.png")
+  ballImage = loadImage("image/pngeg.png")
+  playImage = loadImage("image/deffen.png")
+  goal = loadImage("image/goal.png")
+  pan = loadImage("image/pg.png")
+  pack = loadImage("image/www.png")
+}
+
 function setup() {
-  createCanvas(1250, 600);
+  createCanvas(1250, 1000);
+  swamp = createSprite(625, 280);
+  swamp.scale = 2.5;
+  swamp.addImage(backImage);
+
   edge = createEdgeSprites();
-  player = createSprite(50, 550, 30, 30);
-  player.shapeColor = "yellow";
-  target = createSprite(1200, 50, 30, 30);
-  target.shapeColor = "blue";
-  obs1 = createSprite(250, 200, 100, 30);
-  obs1.shapeColor = "red";
-  obs2 = createSprite(350, 400, 100, 30);
+  player = createSprite(620, 960, 30, 30);
+  player.addImage(ballImage);
+  player.scale=.1
+  target = createSprite(620, 100, 10, 10);
+  // target.shapeColor = "blue";
+  target.addImage(pack)
+  target.scale = .3
+  obs1 = createSprite(250, 230, 100, 30);
+  // obs1.shapeColor = "red";
+  obs1.addImage(playImage)
+  obs1.scale = 0.2
+  obs2 = createSprite(350, 550, 100, 30);
   obs2.shapeColor = "red";
-  obs3 = createSprite(450, 200, 100, 30);
+  obs2.addImage(playImage);
+  obs2.scale = 0.2;
+  obs3 = createSprite(450, 390, 100, 30);
   obs3.shapeColor = "red";
-  obs4 = createSprite(550, 400, 100, 30);
+  obs3.addImage(playImage);
+  obs3.scale = 0.2;
+  obs4 = createSprite(550, 700, 100, 30);
   obs4.shapeColor = "red";
-  obs5 = createSprite(650, 200, 100, 30);
+  obs4.addImage(playImage);
+  obs4.scale = 0.2;
+  obs5 = createSprite(650, 230, 100, 30);
   obs5.shapeColor = "red";
-  obs6 = createSprite(750, 400, 100, 30);
+  obs5.addImage(playImage);   
+  obs5.scale = 0.2;
+  obs6 = createSprite(750, 550, 100, 30);
   obs6.shapeColor = "red";
-  obs7 = createSprite(850, 200, 100, 30);
+  obs6.addImage(playImage);
+  obs6.scale = 0.2;
+  obs7 = createSprite(850, 390, 100, 30);
   obs7.shapeColor = "red";
-  obs8 = createSprite(950, 400, 100, 30);
+  obs7.addImage(playImage);
+  obs7.scale = 0.2;
+  obs8 = createSprite(950, 700, 100, 30);
   obs8.shapeColor = "red";
+  obs8.addImage(playImage);
+  obs8.scale = 0.2;
 
 
   
-    wormGroup= new Group();
+    // wormGroup= new Group();
   
   obs1.velocityX = -20;
   obs2.velocityX = -20;
@@ -77,48 +114,48 @@ function draw() {
   if(player.isTouching(obs1)) {
     obs1.velocityX = 0;
     textSize(30);
-    text("You Loss",580,260);}
+    text("You Loss",580,490);}
     // text("You loose", 200, 300);
     
   if(player.isTouching(obs1)) {
     obs1.velocityX = 0;
     textSize(30);
-    text("You Loss",580,260);}
+    text("You Loss",580,490);}
 
   if(player.isTouching(obs2)) {
     obs2.velocityX = 0;
     textSize(30);
-    text("You Loss",580,260);}
+    text("You Loss",580,490);}
 
   if(player.isTouching(obs3)) {
     obs3.velocityX = 0;
     textSize(30);
-    text("You Loss",580,260);}
+    text("You Loss",580,490);}
 
   if(player.isTouching(obs4)) {
     obs4.velocityX = 0;
     textSize(30);
-    text("You Loss",580,260);}
+    text("You Loss",580,490);}
 
   if(player.isTouching(obs5)) {
     obs5.velocityX = 0;
     textSize(30);
-    text("You Loss",580,260);}
+    text("You Loss",580,490);}
 
   if(player.isTouching(obs6)) {
     obs6.velocityX = 0;
     textSize(30);
-    text("You Loss",580,260);}
+    text("You Loss",580,490);}
 
   if(player.isTouching(obs7)) {
     obs7.velocityX = 0;
     textSize(30);
-    text("You Loss",580,260);}
+    text("You Loss",580,490);}
 
   if(player.isTouching(obs8)) {
     obs8.velocityX = 0;
     textSize(30);
-    text("You Loss",580,260);
+    text("You Loss",580,490);
     }
     
   if(player.isTouching(target)){
@@ -131,46 +168,48 @@ function draw() {
     obs7.velocityX = 0;
     obs8.velocityX = 0;
     textSize(40)
-    text("You Win",580,260)
+    text("You Win",580,490)
     stroke("transparent")
   }
 
 
-    stroke("black")
-    noFill();
-    ellipse(600,280,40,30);
-    player.x= mouseX;
-    player.y= mouseY;
+    // stroke("black")
+    // noFill();
+    // ellipse(600,280,40,30);
+    // player.x= mouseX;
+    // player.y= mouseY;
 
-if(player.x<640 && player.x>570 && player.y<300 && player.y>250 ){
-  text("NO CHEATING!!",600,150);
-  player.x=50;
-  player.y=550;
+// if(player.x<640 && player.x>570 && player.y<300 && player.y>250 ){
+//   text("NO CHEATING!!",230,150);
+//   player.x=625;
+//   player.y=260;
+// }
+
+// generateWorms();
+
+// for(var i = 0 ; i< (wormGroup).length ;i++){
+//   var temp = (wormGroup).get(i) ;
+//   if (player.isTouching(temp)) {
+//     score++;
+//     temp.destroy();
+//     temp=null;
+//     }   
+//   }
+
+  // drawSprites();
+  // textSize(30);
+  // text("Worms destroyed:"+score,350,50);
 }
 
-generateWorms();
-
-for(var i = 0 ; i< (wormGroup).length ;i++){
-  var temp = (wormGroup).get(i) ;
-  if (player.isTouching(temp)) {
-    score++;
-    temp.destroy();
-    temp=null;
-    }   
-  }
-
-  drawSprites();
-  textSize(30);
-  text("Worms destroyed:"+score,350,50);
-}
-
-function generateWorms(){
-if(frameCount % 30===0){
-  console.log(frameCount);
-  var worm = createSprite(600,280,60,8);
-  worm.shapeColor="skyblue"
-  worm.velocityX=random(-15,15);
-  worm.velocityY=random(-15,15);
-  wormGroup.add(worm);
-  }
-}
+// function generateWorms(){
+// if(frameCount % 50===0){
+//   console.log(frameCount);
+//   var worm = createSprite(220,280,60,8);
+//   worm.shapeColor="skyblue"
+//   // worm.addImage("wormImage")
+//   worm.scale = 0.5;
+//   worm.velocityX=random(-10,10);
+//   worm.velocityY=random(-10,10);
+//   wormGroup.add(worm);
+//   }
+// }
